5a262ea2d94c0ed8a767d9a92630fed72e00e1d2
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2016-05-17 07:35:20 +0100

    BoolTests: Added test for true.

---
+++ Src/CSDataTypes.Test/BoolTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSDataTypes.Test
+{
+    [TestClass]
+    public class BoolTests
+    {
+        [TestMethod]
+        public void IsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
