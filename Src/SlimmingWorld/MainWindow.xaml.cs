5ff4623ec9e2100e65fbae12b5d1222271f16520
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2016-05-27 14:07:23 +0100

    Adding some attempts at WPF files.

---
+++ Src/SlimmingWorld/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlimmingWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Items_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Selection.Text = ((ContentControl)((object[])e.AddedItems)[0]).Content.ToString();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button) sender;
+            Selection.Background = button.Background;
+        }
+    }
+}
---
+++ Src/SlimmingWorld/SignalRClient.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SlimmingWorld
+{
+    /// <summary>
+    /// Interaction logic for SignalRClient.xaml
+    /// </summary>
+    public partial class SignalRClient : Window
+    {
+        public SignalRClient()
+        {
+            InitializeComponent();
+            var c = new Connection("http://localhost:29575/chat");
+            var t = c.Start();
+            t.Wait();
+            c.Send("I'm here!");
+        }
+    }
+}
